90dc981aeeed99839896661810e28c03a77926a1
leni lunderman <lenilunderman@gmail.com>
2020-07-16 10:59:31 -0400

    Change Password HTML completed and Change Password Curl Script Completed.
    The next step is to work on the Javascript part, connecting the form with the API.

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -10,14 +10,3 @@ curl 'https://tic-tac-toe-api-production.herokuapp.com/sign-in' \
   }'
 
 echo
-
-# {
-#     "user":{
-#         "_id":"5f0fc59ea25eae00175c6efd",
-#         "email":"lenilunderman@gmail.com",
-#         "createdAt":"2020-07-16T03:12:30.735Z",
-#         "updatedAt":"2020-07-16T03:22:34.242Z",
-#         "__v":0,
-#         "token":"e363dd42841ba7fa496f569a6eecb53f"
-#     }
-# }
